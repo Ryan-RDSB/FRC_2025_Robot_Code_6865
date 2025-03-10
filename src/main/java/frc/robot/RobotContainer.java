90dd57a305918385b3de0d2f732c74b3cca84450
Tanner-hacker-man <sugar0cane0@gmail.com>
2025-03-10 15:01:52 -0400

    120 Direction For Lvl 4 Arm Command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -97,7 +97,7 @@ public class RobotContainer {
         operationsController.a().whileTrue(
             new ParallelCommandGroup(
                 elevator.ElevatorCommand(40),
-                arm.ArmCommand(70)
+                arm.ArmCommand(120)
                 )
             );
         operationsController.leftBumper().onTrue(
